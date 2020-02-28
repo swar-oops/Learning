5bb9a48f6c32add7af6eec47e52f02d1a96b3eae
Swaroop <swaroop.shruti123@gmail.com>
2020-02-28 18:27:29 +0530

    Strings

---
+++ basics.py
@@ -0,0 +1,6 @@
+#strings
+mystring = "Hellobabe"
+print(mystring[::-1]) 
+#(The 2 colons represent the start(left), end(Middle)
+# and the right most as the step index) step index represents the no of skips.
+print(mystring[::2])
