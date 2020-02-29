a00159cc0d6392122797649ce709348195caa006
Swaroop <swaroop.shruti123@gmail.com>
2020-02-29 16:39:44 +0530

    Print formatting of strings

--- format.py
+++ format.py
@@ -2,3 +2,9 @@ print('The string is {}'.format("I'm inside it boys"))
 #float formating {width:value,precisionf}
 
 print('The string is {0} {0} {0}'.format('amazing','great','okay')) 
+print('The string is {a} {c} {b}'.format(a='amazing',b = 'great',c = 'okay'))
+#floating point boys   {value:width.precisionf}
+#result 
+result = 10.213123312
+print('The string is {r:1.4f}'.format(r=result))
+
