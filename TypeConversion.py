d47a22d4bc87d9ed597ffeebae626a0108892901
Swaroop <swaroop.shruti123@gmail.com>
2020-09-01 15:55:55 +0530

    Comment changes

--- TypeConversion.py
+++ TypeConversion.py
@@ -1,9 +1,9 @@
-#converting a data type to a different is known as type casting.
-# Example    
-a = '100'
-b = type(int(a))
-print(b)
-#another way of writing the same thing would look like this:
+#converting a data type to a different type is known as type casting.
+#Example    
+a = '100' #String variable
+b = type(int(a)) #Converting the string variable with the method type() to change the string variable to the type "int" 
+print(b) #Storing the converted type in variable b.
+#Another way implementing the same as above would look like this:
 Type = type(int(str(100)))
 print(Type)
 #Both the results are going to print the same output but, the 1st method declares different variables to help us understand the process of casting in the structural way.
