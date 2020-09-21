99c577ab587db22f7b8c8eba39f02fefbf871f04
Swaroop <swaroop.shruti123@gmail.com>
2020-09-21 23:49:14 +0530

    LIst_slicing

--- Basics 1/List_Slicing.py
+++ Basics 1/List_Slicing.py
@@ -13,8 +13,8 @@ new_cart = amazon_cart[0:2] #Copying the amazon_cart elements till 2nd index to 
 new_cart[1] = 'waffles'
 
 #If we copy the entire items of amazon_cart to new_cart. What would happen? 
-new_cart = amazon_cart #Both the outputs of the cart will have exact same items. This happens because new_cart is assigned to amazon_cart and the values of amazon_cart changes in the memory device.
-new_cart[0] = 'Desktop'
+new_cart = amazon_cart[:] #Both the outputs of the cart will have exact same items. This happens because new_cart is assigned to amazon_cart and the values of amazon_cart changes in the memory device.
+new_cart[0] = "Desktop"
 print(new_cart)
 print(amazon_cart)
 new_cart = amazon_cart[:] #This will make sure to copy all the elements of amazon_cart to new_cart without changing the amazon_cart value. 
