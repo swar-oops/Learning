99426a77bef16cfa454ad6bb4eb57948cd071a84
Swaroop <swaroop.shruti123@gmail.com>
2020-09-23 20:53:39 +0530

    Matrix

---
+++ Basics 1/Matrix.py
@@ -0,0 +1,13 @@
+#Matrix is an array with multiple dimensions. Matrix is useful in most of the computer science concepts.
+matrix2d = [ #2d array with 3 rows & 3 coloumns.
+    [0,1,5],
+    [3,2,4],
+    [4,8,9],
+]
+print(matrix2d[1][2]) #prints the 2nd row and the 3rd coloumns intersection.
+#     0  1  2
+# 0 | 0  1  5 |
+# 1 | 3  2  4 | 
+# 2 | 4  8  9 |
+
+#Visual representation of the matrix2d Array
